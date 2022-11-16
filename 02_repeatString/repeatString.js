4f13d596409c6e6668f4fad17db58498f15b3105
buksimesi <buksimesi18@gmail.com>
2022-11-16 22:12:32 +0200

    :white_check+mark: solve the first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(mystr, num) {
+  let i = 0
+  let res = mystr
+  while(i < num-1) {
+    res += mystr;
+    i++;
+  }
+  return res;
 };
 
 // Do not edit below this line
