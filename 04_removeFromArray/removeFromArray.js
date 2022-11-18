e6d4f8ac41f828e71aa69e9b8a863d05ad86f5c8
buksimesi <buksimesi18@gmail.com>
2022-11-18 16:11:58 +0200

    :white_check_mark: solve the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArr, ...args) {
+  for(let i=0; i<args.length; i++) {
+    if(myArr.includes(args[i])){
+      myArr.splice(myArr.indexOf(args[i]),1)
+    }
+  }
+  return myArr;
 };
 
 // Do not edit below this line
